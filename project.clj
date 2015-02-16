74e688d9c1611bee2073817990f1c8a78dcd859e
Daniel Stone <dastone@microsoft.com>
2015-02-16 16:17:57 +0000

    added release props

--- project.clj
+++ project.clj
@@ -7,4 +7,5 @@
                  [clj-http "1.0.1"]
                  [camel-snake-kebab "0.3.0" :exclusions [org.clojure/clojure]]]
 
-  :plugins [[lein-release "1.0.5"]])
+  :plugins [[lein-release "1.0.5"]]
+  :lein-release {:deploy-via :clojars})
